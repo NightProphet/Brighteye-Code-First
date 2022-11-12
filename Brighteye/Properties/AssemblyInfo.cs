e5e4463cce4365131fad5d26478551afb3837d5f
Adil Grigorev <n.p.github@gmail.com>
2022-11-12 15:43:17 +0500

    Update AssemblyInfo.cs
    Updated copyright information

--- Brighteye/Properties/AssemblyInfo.cs
+++ Brighteye/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Brighteye")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright © 2022 Adil Grigorev")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
